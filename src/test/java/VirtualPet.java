a71c1f55b44853aadadd7f908ab2858b5e5d1117
quincyrogers <quincyrogers>
2019-02-03 15:12:40 -0500

    initial amok push

---
+++ src/test/java/VirtualPet.java
@@ -0,0 +1,27 @@
+
+public class VirtualPet {
+
+	private String name;
+	private String breed;
+	private String cage;
+
+
+	public VirtualPet(String name, String breed, String cage) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBreed() {
+		// TODO Auto-generated method stub
+		return breed;
+	}
+
+	public String getCage() {
+		// TODO Auto-generated method stub
+		return cage;
+	}
+
+}
